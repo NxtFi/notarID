af57390544ea4d9419d43e5eb02155dff05bb281
Diego Theriano <dtheriano@gmail.com>
2022-07-18 08:28:36 -0300

    data block updated

--- src/components/hashing.js
+++ src/components/hashing.js
@@ -166,21 +166,32 @@ export default function HashingForm(){
         e.preventDefault();
     }
 
-    const handleButton = (e) => {
+    const handleButtonVerificar = (e) => {
         // Solicitud GET (Request).
-        fetch('https://api.github.com/users/manishmshiva')
+        fetch('https://development-001-node.test.nxtfi.net/7489cf6d4c588125eb62e1fff365d4ec8c00e1ebd61bd67f158efe8916765f99/_/'+output)
         // Exito
         .then(response => response.json())  // convertir a json
-        .then(json => console.log(json))    //imprimir los datos en la consola
+        .then(json => {
+            console.log(json);
+            if (json!=null){
+                console.log('hash existente');
+            }
+        
+        })    //imprimir los datos en la consola
         .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+
     }
     
     const handleButtonSellar = async (e) => {
         // datos mandados con la solicutud POST
+        let data_raw = '{"block": {"data": "// IMPORT 7489cf6d4c588125eb62e1fff365d4ec8c00e1ebd61bd67f158efe8916765f99\n { hash: \'1355d4c778090809336ce9d0980af78c16edf218ded10c2a7ac1736c9e8b16fa\' }","by": "NOTARIO","scope": "7489cf6d4c588125eb62e1fff365d4ec8c00e1ebd61bd67f158efe8916765f99"}}';
+        let data = '{"block":{"data":"// IMPORT 7489cf6d4c588125eb62e1fff365d4ec8c00e1ebd61bd67f158efe8916765f99\\n {hash:\'1355d4c778090809336ce9d0980af78c16edf218ded10c2a7ac1736c9e8b16fa\'}","by":"NOTARIO","scope":"7489cf6d4c588125eb62e1fff365d4ec8c00e1ebd61bd67f158efe8916765f99"}}';
+
         let _data = {
-            data: "220eddabf3918da723f8b4b1efea5f13d993ee86c8c463f6534a8acdd091d783",
+            //data: output,
+            data: "// IMPORT 7489cf6d4c588125eb62e1fff365d4ec8c00e1ebd61bd67f158efe8916765f99\\n { hash: \"1355d4c778090809336ce9d0980af78c16edf218ded10c2a7ac1736c9e8b16fa\" }",
 		    by: "NOTARIO",
-		    scope: "NOTARIO"
+		    scope: "7489cf6d4c588125eb62e1fff365d4ec8c00e1ebd61bd67f158efe8916765f99"
         }
         
         // fetch('https://development-signblock.test.nxtfi.net/create', {
@@ -194,21 +205,23 @@ export default function HashingForm(){
         // .then(json => console.log(json))
         // .catch(err => console.log(err));
 
-        const location = 'development-signblock.test.nxtfi.net';
+        const location = 'signblock.test.nxtfi.net';
         const settings = {
             method: 'POST',
-            'mode': 'no-cors',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(_data)
+            //mode: 'no-cors',
+            headers: {"Content-type": "application/json"},
+            //body: JSON.stringify(data_raw)
+            body: data
         };
+        //console.log(JSON.stringify(data_raw));
+        console.log(data);
         try {
             const fetchResponse = await fetch(`https://${location}/create`, settings);
             const data = await fetchResponse.json();
+            console.log('Resultado: ', data);
             return data;
         } catch (e) {
+            console.log('Error: ',  e);
             return e;
         }    
 
@@ -266,7 +279,7 @@ export default function HashingForm(){
                 }
                 {showMessage &&
                 <div className="hashed-button">              
-                    <button className="space" type="button" onClick={handleButton}>VERIFICAR</button>
+                    <button className="space" type="button" onClick={handleButtonVerificar}>VERIFICAR</button>
                     <button type="button" onClick={handleButtonSellar}>SELLAR</button>
                 </div>
                 }       
