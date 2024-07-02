9856ae3229b8c10602ae0d5ff297a848d6a4190e
Juan Robles <robles.juanc@gmail.com>
2024-07-01 22:37:14 -0400

    Update ShowResponse.js

--- src/components/ShowResponse.js
+++ src/components/ShowResponse.js
@@ -49,24 +49,6 @@ const ShowResponse = ({ showResponse, backToInitialState }) => {
                 <h3 className="font-semibold">Hash del bloque:</h3>
                 <p className="text-sm"><a style="color: cornflowerblue;text-decoration: underline;" target="_blank" href={"https://demo-001-node.cloud.nxtfi.org/v2/_block/" + showResponse.data.hash}>{showResponse.data.hash}</a></p>
               </div>
-              {
-                showResponse.data.name !== "" ? <div className="grid gap-1">
-                <h3 className="font-semibold">Name:</h3>
-                <p className="text-sm">showResponse.data.name showResponse.data.lastName</p>
-                </div> : <div></div>
-              }
-              {
-                showResponse.data.docnumber !== "" ? <div className="grid gap-1">
-                <h3 className="font-semibold">Document:</h3>
-                <p className="text-sm">showResponse.data.doctype showResponse.data.docnumber</p>
-                </div> : <div></div>
-              }
-              {
-                showResponse.data.obs !== "" ? <div className="grid gap-1">
-                <h3 className="font-semibold">Name:</h3>
-                <p className="text-sm">showResponse.data.obs</p>
-                </div> : <div></div>
-              }
               <div className="grid gap-1">
                 <h3 className="font-semibold">Timestamp:</h3>
                 <p className="text-sm">{showResponse.data.timestamp}</p>
