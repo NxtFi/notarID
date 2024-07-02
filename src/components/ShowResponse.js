e7aa15be5704cf1aa4b7d655e4c7ca4790d95b0a
Juan Robles <robles.juanc@gmail.com>
2024-07-01 22:55:10 -0400

    Update ShowResponse.js

--- src/components/ShowResponse.js
+++ src/components/ShowResponse.js
@@ -52,19 +52,19 @@ const ShowResponse = ({ showResponse, backToInitialState }) => {
               {
                 showResponse.data.name !== "" ? <div className="grid gap-1">
                 <h3 className="font-semibold">Name:</h3>
-                <p className="text-sm">showResponse.data.name showResponse.data.lastName</p>
+                <p className="text-sm">{showResponse.data.name showResponse.data.lastName}</p>
                 </div> : <div></div>
               }
               {
                 showResponse.data.docnumber !== "" ? <div className="grid gap-1">
                 <h3 className="font-semibold">Document:</h3>
-                <p className="text-sm">showResponse.data.doctype showResponse.data.docnumber</p>
+                <p className="text-sm">{showResponse.data.doctype showResponse.data.docnumber}</p>
                 </div> : <div></div>
               }
               {
                 showResponse.data.obs !== "" ? <div className="grid gap-1">
                 <h3 className="font-semibold">Name:</h3>
-                <p className="text-sm">showResponse.data.obs</p>
+                <p className="text-sm">{showResponse.data.obs}</p>
                 </div> : <div></div>
               }
               <div className="grid gap-1">
